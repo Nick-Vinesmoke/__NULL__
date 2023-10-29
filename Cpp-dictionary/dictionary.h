0c356963c56964ed69c2ef5c4b0cb04a35d67a31
Nick Vinesmoke <123866627+Nick-Vinesmoke@users.noreply.github.com>
2023-10-29 09:51:54 +0200

    Delete Cpp-dictionary directory

--- Cpp-dictionary/dictionary.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include"main.h"
-#include "topic.h"
-
-bool LoadList(Topic& mainTopic, const std::string& path);
--- Cpp-dictionary/helper.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "main.h"
-#include<vector>
-
-
-std::string GetUser();
-std::vector<std::string> Split(const std::string& input, const std::string& separator);
-void toLower(std::string& line);
